21d5ec80ed628cbf9c7652e4d8deba0851e0a530
kai <baialievakaiyrgul@gmail.com>
2024-12-25 22:40:21 +0600

    first commit

---
+++ src/main/java/Exam/Edabit.java
@@ -0,0 +1,9 @@
+package Exam;
+
+public class Edabit {
+    //This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+    public static boolean returnTrue() {
+     return true;
+        }
+    }
+
---
+++ src/main/java/org/digital_nomads/Main.java
@@ -0,0 +1,7 @@
+package org.digital_nomads;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
